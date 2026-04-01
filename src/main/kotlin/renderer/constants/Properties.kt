7581a87e16964704269955643d3dd09d88827b37
Hiroki <hiroki.watanabe@student.ateneo.edu>
2026-04-01 14:43:36 +0800

    Created Properties.kt to store drawing properties

---
+++ src/main/kotlin/renderer/constants/Properties.kt
@@ -0,0 +1,9 @@
+package renderer.constants
+
+import java.awt.Color
+
+object Properties {
+    var OUTLINE_WIDTH = 0.1.toFloat()
+    var OUTLINE_COLOR = Color.BLACK
+    var FILL_COLOR = Color.GRAY
+}
