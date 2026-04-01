982081df39d76b7f9cfe590dcb167750f91ab78f
Hiroki <hiroki.watanabe@student.ateneo.edu>
2026-04-01 13:18:15 +0800

    Created enum classes Direction.kt and LightType.kt in preparation for future operations

---
+++ src/main/kotlin/renderer/constants/Direction.kt
@@ -0,0 +1,5 @@
+package renderer.constants
+
+enum class Direction {
+    RIGHT, LEFT, UP, DOWN, FORWARD, BACKWARD
+}
---
+++ src/main/kotlin/renderer/constants/LightType.kt
@@ -0,0 +1,5 @@
+package renderer.constants
+
+enum class LightType {
+    AMBIENT, POINT, DIRECTIONAL, SPOT
+}
