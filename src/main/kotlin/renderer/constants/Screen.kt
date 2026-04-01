f8da5d50f8ab38b319a31c8fb4e400b851be8349
Hiroki <hiroki.watanabe@student.ateneo.edu>
2026-04-01 12:48:28 +0800

    created Screen.kt to contain constants regarding screen size and refresh rate

---
+++ src/main/kotlin/renderer/constants/Screen.kt
@@ -0,0 +1,8 @@
+package renderer.constants
+
+object Screen {
+    var WIDTH = 800
+    var HEIGHT = 600
+    const val MILISECONDS_PER_FRAME = 25
+    const val FRAMES_PER_SECOND = 1000.0/MILISECONDS_PER_FRAME
+}
--- src/main/kotlin/renderer/window/Frame.kt
+++ src/main/kotlin/renderer/window/Frame.kt
@@ -1,14 +1,11 @@
 package renderer.window
 
+import renderer.constants.Screen
 import javax.swing.JFrame
 import javax.swing.Timer
 
 class Frame: JFrame() {
     private val canvas = Canvas()
-    companion object {
-        val MILLIS_PER_FRAME = 20
-        val FRAMES_PER_SECOND = 1/(MILLIS_PER_FRAME/1000.0)
-    }
 
     fun setUpGUI() {
         title = "3D Renderer"
@@ -21,8 +18,8 @@ class Frame: JFrame() {
     }
 
     private fun startAnimation() {
-        Timer(MILLIS_PER_FRAME) {
-            canvas.updatePositions(MILLIS_PER_FRAME)
+        Timer(Screen.MILISECONDS_PER_FRAME) {
+            canvas.updatePositions(Screen.MILISECONDS_PER_FRAME)
             canvas.repaint()
         }.start()
     }
